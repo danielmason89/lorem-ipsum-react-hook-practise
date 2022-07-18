53e9001f54eeab090e0ef8b713d839c2d79f35f5
Daniel Mason <danmasson0@gmail.com>
2022-07-18 13:47:06 -0400

    Updates

---
+++ precache-manifest.b6de679a886aa743bb4d5ad2c8958f9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9def5f374cebe20aeda70d6fd52b1a3e",
+    "url": "/lorem-ipsum-react-hook-practise/index.html"
+  },
+  {
+    "revision": "b739779a784259c1a4df",
+    "url": "/lorem-ipsum-react-hook-practise/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "61c9a72a86221f692a54",
+    "url": "/lorem-ipsum-react-hook-practise/static/js/2.611ee867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lorem-ipsum-react-hook-practise/static/js/2.611ee867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b739779a784259c1a4df",
+    "url": "/lorem-ipsum-react-hook-practise/static/js/main.9f384e5a.chunk.js"
+  },
+  {
+    "revision": "76dfffb09a615c8b25ab",
+    "url": "/lorem-ipsum-react-hook-practise/static/js/runtime-main.d3f80c71.js"
+  }
+]);
